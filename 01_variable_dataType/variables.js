fc598ff43eca3c5395cdfebcd63336133c30a4b3
Igor Zverev <zis19901@gmail.com>
2024-08-12 13:25:38 -0500

    first assignment

---
+++ 01_variable_dataType/variables.js
@@ -0,0 +1,18 @@
+let name = 'Igor Zverev';
+
+
+let age = 34;
+
+let isStudent = true;
+
+let favoriteColors = [ 'Red', 'Blue', 'Orange' ];
+
+let person = {
+    firstName :'Igor',
+    lastName : 'Zverev',
+    age : 34
+}
+
+
+console.log(typeof person);
+
