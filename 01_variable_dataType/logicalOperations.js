6a2c44635c21642e5f7f13a7b4983e08871fa6d4
Igor Zverev <zis19901@gmail.com>
2024-08-13 12:20:30 -0500

    2nd Assignment

--- 01_variable_dataType/logicalOperations.js
+++ 01_variable_dataType/logicalOperations.js
@@ -8,6 +8,7 @@ console.log("If the customer is eligible for shipping?", eligibleForFreeShipping
 
 
 
+let num = 0;
+let isNotZero = !!num;
 
-
-
+console.log("Price is not zero", isNotZero);
---
+++ 01_variable_dataType/ternaryOperations.js
@@ -0,0 +1,13 @@
+//let age = 12;
+
+//let message = (age >= 21) ? "Old enogh to vote" : "Not old enough to vote";
+
+//console.log(message);
+
+
+let price = 71;  
+
+let message = (price > 100) ? "The product is on sale" : "The product is not on sale";
+
+
+console.log(message);
