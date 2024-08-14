a4677cd932778cfe008c0644460077d10c5743d7
Igor Zverev <zis19901@gmail.com>
2024-08-14 16:23:39 -0500

    5th assignment

---
+++ 03_controlFlow/ifElseStatement.js
@@ -0,0 +1,95 @@
+// let number = 7;
+
+// if( number % 2 == 0) {
+// console.log(`${number} is even`)
+// } else {
+// console.log(`${number} is odd`)
+// };
+
+
+// Write a program based on age and displays a message based on their age using an if-else if statement:
+
+// let age = 55;
+
+// if (age < 18){
+//     console.log(`You are a minor: ${age}`)
+// } else if (age >=65) {
+//     console.log(`You are a senior citizen: ${age}`)
+// } else {
+//     console.log(`"You are an adult" ${age}`)  
+// };
+
+
+// Write a program based on a letter and displays whether the letter is a vowel or a consonant using an if-else statement.
+
+// let letter = 'P';
+
+// letter = letter.toLocaleLowerCase();
+
+// if (letter === 'a' || letter === 'e' || letter === 'i' || letter === 'o' || letter === 'u') {
+//     console.log(`${letter} is a vowel`);
+// } else {
+//     console.log(`${letter} is a consonant`);
+// };
+
+
+// Write a program based on three numbers and displays the largest number using an if-else statement.
+let numberOne = 11;
+let numberTwo = 2;
+let numberThree = 322;
+
+// if(numberOne > numberTwo && numberOne > numberThree) {
+// console.log (`This is the largest number: ${numberOne}`)
+// } else if( numberTwo > numberOne && numberTwo > numberThree){
+//     console.log (`This is the largest number: ${numberTwo}`)
+// } else {
+//     console.log (`This is the largest number: ${numberThree}`)
+// };
+
+// let largest;
+
+// if(numberOne > numberTwo && numberOne > numberThree) {
+//    largest = numberOne
+//     } else if( numberTwo > numberOne && numberTwo > numberThree){
+//         largest = numberTwo
+//     } else {
+//         largest = numberThree
+//     };
+    
+// console.log(`Largest number is ${largest}`);
+
+// Write a program based on given password and
+// displays a message based on the password using an if-else statement:
+
+// If the password is less than 8 characters, display "Password too short".
+// If the password contains both letters and numbers, display "Password accepted".
+// If the password does not meet either of the above conditions, display "Password rejected".
+
+
+// let password = '1234232343';
+
+// password = password.toLocaleLowerCase();
+
+// if(password.length < 8){
+//     console.log(`Password is too short`)
+// } else if (/\d/.test(password) && /[a-z]/.test(password)) {
+//     console.log(`Password accepted`) 
+// } else {
+//     console.log(`Password rejected`) 
+// };
+
+// let password = 'Test123d3'; 
+
+// if (password.length < 8) {
+//     console.log("Password is too short");
+// } else if (/[a-zA-Z]/.test(password) && /\d/.test(password)) {
+//     console.log("Password accepted");
+// } else {
+//     console.log("Password rejected");
+// }
+
+
+
+
+
+
