e50b46e7fe1696455ad48485e8c5ef5be5bd1988
Igor Zverev <zis19901@gmail.com>
2024-08-14 14:50:18 -0500

    4th Assignment

---
+++ 01_variable_dataType/arrayExamples.js
@@ -0,0 +1,53 @@
+let students = ['Igor', 'Johny', 'Max', 'Kate', 'Alex'];
+
+// console.log(students[0]);
+
+
+// console.log(students.length);
+
+
+// let removeStudent = students.pop();
+// // console.log('After removal', students);
+// console.log('Student removed', removeStudent);
+
+
+// let removedElements = students.splice(0, 2);
+// console.log('After splice to remove:', students);
+// console.log('Removed Elements:', removedElements);
+
+
+// let poppedStudent = students.pop();
+// console.log ('Removed student', poppedStudent);
+// console.log (`New list of students ${students}`);
+
+
+// let hasJohn = students.includes('John'); 
+// console.log('Array includes John', hasJohn);
+
+
+// console.log(students.indexOf("Kate"));
+
+
+// console.log(students.join());
+
+
+// let reverse = students.reverse();
+// console.log(students);
+
+
+// students.reverse();
+// console.log(students);
+
+
+let firstIndex = students[0];
+let secondIndex = students[4]
+let isFirstIndexEqualToLastIndex = firstIndex == secondIndex;
+console.log("Values of the first and last indecies are equal", isFirstIndexEqualToLastIndex);
+
+
+
+
+
+
+
+
